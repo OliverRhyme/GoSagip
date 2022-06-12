db46794932a8f9cfb3c7493465a8e06f78c82477
Oliver Rhyme Guibone Añasco <35908542+OliverRhyme@users.noreply.github.com>
2022-06-12 19:08:54 +0800

    Implement edge to edge

--- app/src/main/java/dev/rhyme/gosagip/ui/MainActivity.kt
+++ app/src/main/java/dev/rhyme/gosagip/ui/MainActivity.kt
@@ -1,15 +1,17 @@
-package dev.rhyme.gosagip
+package dev.rhyme.gosagip.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import dev.rhyme.gosagip.ui.theme.GoSagipTheme
 
@@ -17,11 +19,13 @@ import dev.rhyme.gosagip.ui.theme.GoSagipTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             GoSagipTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize()
+                        .statusBarsPadding(),
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
--- app/src/main/java/dev/rhyme/gosagip/ui/theme/Theme.kt
+++ app/src/main/java/dev/rhyme/gosagip/ui/theme/Theme.kt
@@ -1,18 +1,10 @@
 package dev.rhyme.gosagip.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
+private val Palette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
@@ -28,12 +20,8 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun GoSagipTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun GoSagipTheme(content: @Composable () -> Unit) {
+    val colors = Palette
 
     MaterialTheme(
         colors = colors,
